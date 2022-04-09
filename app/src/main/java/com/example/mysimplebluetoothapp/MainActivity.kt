c839066654a431f5da534d9776a9ea5b0877bf54
Joanna <joanna.tr48@gmail.com>
2022-04-09 09:58:40 +0200

    SimpleBluetooth App - Zugriff auf die Liste der bereits gekoppelten Geräte - Problem in MainActivity.kt: "R.layout.support_simple_spinner_dropdown_item"

--- app/src/main/java/com/example/mysimplebluetoothapp/MainActivity.kt
+++ app/src/main/java/com/example/mysimplebluetoothapp/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.mysimplebluetoothapp
 
+
 import android.bluetooth.BluetoothAdapter
 import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 import splitties.toast.toast
 
 class MainActivity : AppCompatActivity() {
 
-    private val btnConnectedDevices : Button by lazy { findViewById(R.id.ConnectedDevicesButton) }
+    private val btnPairedDevices : Button by lazy { findViewById(R.id.PairedDevicesButton) }
     private val btnBluetoothConnection : Button by lazy { findViewById(R.id.searchBluetoothConnectionButton) }
-    private val lvConnectedDevices : ListView by lazy{ findViewById(R.id.ConnectedDevicesListView) }
+    private val lvPairedDevices : ListView by lazy{ findViewById(R.id.PairedDevicesListView) }
 
     private lateinit var mBluetooth: BluetoothAdapter
 
@@ -22,23 +24,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnConnectedDevices.setOnClickListener {
-            Log.i(TAG, getString(R.string.ConnectedDummyText))
-            toast(getString(R.string.ConnectedDummyText))
-        }
-
-
-        btnBluetoothConnection.setOnClickListener {
-            Log.i(TAG, getString(R.string.SearchDummyText))
-            toast(getString(R.string.SearchDummyText))
-        }
-
         mBluetooth = BluetoothAdapter.getDefaultAdapter()
         if(mBluetooth == null)
         {
             toast(getString(R.string.btNotAvailable))
             finish();
         }
+
+        btnPairedDevices.setOnClickListener {
+            Log.i(TAG, getString(R.string.ConnectedDummyText))
+            toast(getString(R.string.ConnectedDummyText))
+            getPairedDevices()
+        }
+
+        btnBluetoothConnection.setOnClickListener {
+            Log.i(TAG, getString(R.string.SearchDummyText))
+            toast(getString(R.string.SearchDummyText))
+        }
+
     }
 
 
@@ -48,5 +51,24 @@ class MainActivity : AppCompatActivity() {
             val turnBTOn = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(turnBTOn, 1)
         }
+    }
+
+    private fun getPairedDevices() {
+
+        val pairedDevices = mBluetooth!!.bondedDevices
+        val list = ArrayList<Any>()
+
+        if (pairedDevices.size > 0) {
+            for (bt in pairedDevices) {
+                list.add("""${bt.name}${bt.address}""".trimIndent())
+            }
+        } else {
+            toast(getString(R.string.btNoPairedDevices))
+        }
+
+        val adapter: ArrayAdapter<*> = ArrayAdapter(this,
+            R.layout.support_simple_spinner_dropdown_item,
+            list)
+        lvPairedDevices.adapter = adapter
     }
 }
